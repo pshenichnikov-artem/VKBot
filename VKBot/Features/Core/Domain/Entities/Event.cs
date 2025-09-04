f67e77511a7a7742eba47e8692a1f8482ff06b12
11RIKI11 <159634566+11RIKI11@users.noreply.github.com>
2025-09-04 08:46:25 +0300

    Entitities added

---
+++ VKBot/Features/Core/Domain/Entities/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VKBot.Features.Core.Domain.Enums;
+
+namespace VKBot.Features.Core.Domain.Entities
+{
+    public class Event
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public long MessageId { get; set; } //?
+        public long AdminId { get; set; } //?
+        public User? Admin { get; set; } = null;
+        [Required]
+        public string Type { get; set; } = TargetType.All.ToString().ToLower();
+        public string? TargetGroup { get; set; } = null;
+    }
+}
---
+++ VKBot/Features/Core/Domain/Entities/Inbox.cs
@@ -0,0 +1,11 @@
+﻿namespace VKBot.Features.Core.Domain.Entities
+{
+    public class Inbox
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public User? User { get; set; } = null;
+        public long MessageId { get; set; }
+        public short CountRetry { get; set; } = 0;
+    }
+}
