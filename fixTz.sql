2ade306ecd5fd2082b3ce36c2d5155d326575467
11RIKI11 <159634566+11RIKI11@users.noreply.github.com>
2025-09-07 20:04:40 +0300

    ав

--- fixTz.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "Messages" 
-ALTER COLUMN "LastReminderSent" TYPE timestamp without time zone USING "LastReminderSent"::timestamp without time zone;
-
-ALTER TABLE "MessageDeliveries"
-ALTER COLUMN "DispatchTime" TYPE timestamp without time zone USING "DispatchTime"::timestamp without time zone;
